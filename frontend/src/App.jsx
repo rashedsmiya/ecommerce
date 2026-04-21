1fcfc911938d803508dfb8811a96611927b74a99
rashedsmiya <rashedmiyaofficial@gmail.com>
2026-04-22 01:57:16 +0600

    up

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Shop from "./components/Shop";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/shop' element={<Shop/>}/>
+            </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Home = () => {
+    return (
+        <>
+          <header className="shadow">
+            <data className="bg-dark text-center py-3">
+                <span className="text-white">Your fashion partner</span>
+            </data>
+          </header>
+        </>
+    );
+}
+export default Home;
