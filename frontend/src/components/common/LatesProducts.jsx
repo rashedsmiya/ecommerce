6b1b6ba4eb1260e991f1a7a1030e024d1b28e6b5
rashedsmiya <rashedmiyaofficial@gmail.com>
2026-04-25 01:27:40 +0600

    up

--- frontend/src/components/common/LatesProducts.jsx
+++ frontend/src/components/common/LatesProducts.jsx
@@ -7,7 +7,7 @@ const LatestProducts = () => {
                   <div className="container">
                       <h2>New Arrivals</h2>
                       <div className="row mt-4">
-                          <div className="col-md-3">
+                          <div className="col-md-3 col-6">
                               <div className="product card border-0">
                                   <div className="card-img">
                                       <img src={ProductImg}  alt=""  className="w-100"/>
@@ -20,21 +20,7 @@ const LatestProducts = () => {
                                   </div>
                               </div>
                           </div>
-                           <div className="col-md-3">
-                              <div className="product card border-0">
-                                  <div className="card-img">
-                                      <img src={ProductImg}  alt=""  className="w-100"/>
-                                  </div>
-                                  <div className="card-body pt-3">
-                                     <a href="">Red Check Shirt for Men</a>
-                                     <div className="price">
-                                        $50 <span className="text-decoration-line-through">$80</span>
-                                     </div>
-                                  </div>
-                              </div>
-                          </div>
-
-                           <div className="col-md-3">
+                           <div className="col-md-3 col-6">
                               <div className="product card border-0">
                                   <div className="card-img">
                                       <img src={ProductImg}  alt=""  className="w-100"/>
@@ -48,7 +34,21 @@ const LatestProducts = () => {
                               </div>
                           </div>
 
-                           <div className="col-md-3">
+                           <div className="col-md-3 col-6">
+                              <div className="product card border-0">
+                                  <div className="card-img">
+                                      <img src={ProductImg}  alt=""  className="w-100"/>
+                                  </div>
+                                  <div className="card-body pt-3">
+                                     <a href="">Red Check Shirt for Men</a>
+                                     <div className="price">
+                                        $50 <span className="text-decoration-line-through">$80</span>
+                                     </div>
+                                  </div>
+                              </div>
+                          </div>
+
+                           <div className="col-md-3 col-6">
                               <div className="product card border-0">
                                   <div className="card-img">
                                       <img src={ProductImg}  alt=""  className="w-100"/>
